eb09dd3dc4c2178c711898eece36c6de348dc1a0
Sajadbasim97 <105778858+Sajadbasim97@users.noreply.github.com>
2022-06-14 03:10:26 +0300

    Update story.php

--- story.php
+++ story.php
@@ -1,20 +1,17 @@
 <?php
-$API_KEY = ""; 
-define("API_KEY", $API_KEY);
-function bot($method, $datas = [])
-{
-    $url = "https://api.telegram.org/bot" . API_KEY . "/" . $method;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+ob_start();
+define('API_KEY','5538420645:AAGopAdED2NRvTFUp7g_QsV23brUTX3QA-M');
+echo "https://api.telegram.org/bot".API_KEY."/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME'];
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+$ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
     $res = curl_exec($ch);
-    if (curl_error($ch)) {
+    if(curl_error($ch)){
         var_dump(curl_error($ch));
-    } else {
-        return json_decode($res);
-    }
-}
+    }else{        return json_decode($res);    }}
 
 $update = json_decode(file_get_contents('php://input'));
 $message = $update->message;
@@ -24,64 +21,36 @@ $chat_id2 = $update->callback_query->message->chat->id;
 $message_id = $update->callback_query->message->message_id;
 $data = $update->callback_query->data;
 $from_id = $message->from->id;
-$u = explode("\n",file_get_contents("memb.txt"));
-$c = count($u)-1;
-$modxe = file_get_contents("usr.txt");
-$admin =5555 ;
-$sudo = 175279237;
-$name = $update->message->from->first_name;
-$username = $update->message->from->username;
-$t =$message->chat->title;
-$user = $message->from->username;
-$ch = "@c_941";#معرف قناتك
-$join = file_get_contents("https://api.telegram.org/bot".API_KEY."/getChatMember?chat_id=$ch&user_id=".$from_id);
-if($message && (strpos($join,'"status":"left"') or strpos($join,'"Bad Request: USER_ID_INVALID"') or strpos($join,'"status":"kicked"'))!== false){
-bot('sendMessage', [
-'chat_id'=>$chat_id,
-'text'=>"▫️ يجب عليك الاشتراك في قناة البوت اولا ⚜️؛
-◼️ اشترك في القناة ثم ارسل /start ،
- - قناة البوت @c_941 •",
- ]);return false;}  
 if($text == '/start'){
     bot('sendMessage',[
       'chat_id'=>$chat_id,
-               'text' => "- اهلا بك ؛ [$name](tg://user?id=$chat_id)
-⚜¦ فـي بوت تحميل ستـوري انستـا ؛
-🌐¦ البوت الاول فـي تليجرام ؛ 
-📥¦ يـمكنك تـحميل سـتوري الحسابات العامـة فقط ؛
-🔱¦ فـقط ارسـل يوزر الحـساب ؛
-✔️¦ وسيقـوم البـوت بجـلب الستـوري ؛
-﹎﹎﹎﹎﹎﹎﹎﹎﹎﹎﹎﹎
-📌¦ مـلاحضـة اذا لـم يرد عـليك البوت فأن الحسـاب لا يوجد فيـة ستـوري ‼️'
-📮¦ آرسل اليوزر بـدون (  @ )
-﹎﹎﹎﹎﹎﹎﹎﹎﹎﹎﹎﹎
-[• اضغط هنا وتابع جديدنا ، 📢 '](https://t.me/mroan1235)",
-'parse_mode' => "MarkDown", 'disable_web_page_preview' => true, 'reply_markup' => json_encode(['inline_keyboard' => [[['text' => "• اضغط هنا وتابع قناة البوت ، 🔰'", 'url' => "https://t.me/mroan1235"]], ]]) ]);
-}
+      'text'=>' get text or us
+- ','reply_markup'=>json_encode([
+  'inline_keyboard'=>[
+   [['text'=>'ok tv me  ،','url'=>'t.me/ss_ss']],]  ]) ]);}
+
 if($text != '/start' ){
-$api = file_get_contents("https://www.instagram.com/$text/?__a=1");
-            if (!empty($api) and strpos($api,"Sorry, this page isn't available.") === false){
-                $api = json_decode($api);
-                bot("SendPhoto",[
-                    "chat_id"=>$chat_id,
-                    "photo"=>$api->stories->image_url,
+ $get = json_decode(file_get_contents("https://storiesdown.com/api/stories/0051ac727b8da9ba1e8a7ae9792c549e?_username=$text"));
+ if($get->error == 'username'){
+   bot('sendMessage',[
+  'chat_id'=>$chat_id,
   'text'=>'ليس هناك أي مستخدم بهذا الأسم ❌',
   'reply_markup'=>json_encode([
     'inline_keyboard'=>[
-        [['text'=>'《 ἿȒᾋQ ἿƝṨҬᾋ》','url'=>'t.me/c_941']]    ]  ]) ]);}
+        [['text'=>'ok','url'=>'t.me/ss_ss']]    ]  ]) ]);}
  if($get->stories->error == 'Not found'){
  bot('sendphoto',[
   'chat_id'=>$chat_id,
-  'photo'=>$get->user->profile_pic_url_hd,
+  'photo'=>$get->stories->image_url,
   'caption'=>'الحساب الذي أرسلتة لم ينشر اي قصة 😬',
   'reply_markup'=>json_encode([
     'inline_keyboard'=>[
-        [['text'=>'《 ἿȒᾋQ ἿƝṨҬᾋ》','url'=>'t.me/c_941']]  ]])]);
+        [['text'=>'ok','url'=>'t.me/ss_ss']]  ]])]);
  } else {
     bot('sendMessage',[
   'chat_id'=>$chat_id,
   'text'=>'يرجى الأنتضار قليلاً 🕒',]);
-    $ge = file_get_contents('https://wapis.ga/insta/getstories/'.$text);
+    $ge = file_get_contents('https://storiesdown.com/api/stories/0051ac727b8da9ba1e8a7ae9792c549e?_username='.$text);
     $json = json_decode($ge, true);
     for ($i=0; $i < count($json['result']['reels_media'][0]['items']); $i++) {
         $photo = $json['result']['reels_media'][0]['items'][$i]['display_url'];
@@ -102,25 +71,4 @@ $api = file_get_contents("https://www.instagram.com/$text/?__a=1");
 يمكنك أرسال اسم مستخدم اخر لتنزيل حالاته او الخروج ⬇️',
 'reply_markup'=>json_encode([
     'inline_keyboard'=>[
-        [['text'=>' 《 ἿȒᾋQ ἿƝṨҬᾋ》','url'=>'t.me/c_941']]    ]  ])]); }}
- 
-if($text == "/start") {
-bot('sendmessage',[ 
-'chat_id'=>$chat_id,
-'text'=>"
- ",
-'reply_to_message_id'=>$message->message_id,
-]);
-bot('sendMessage',[
-'chat_id'=>$sudo,
-'text'=>"
-  ٭هذا دخل للبوت 🔰؛
-• المعرف ؛ @$username ،
-• الاسم ؛ $name 
-• الايدي ؛ $from_id
-• اليوم ؛ " . date("j") . "\n" . "
-",
-]);
-}
-
-
+        [['text'=>'ok','url'=>'t.me/ss_ss']]    ]  ])]); }}
